5468fde554f4c457d8310392b61cc2f9c54b2b45
me-elsa <elsaa.butt@gmail.com>
2024-02-20 01:16:32 +0500

    registration process

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-import userModel from "../models/userModel";
-
+import userModel from "../models/userModel.js";
+import { hashPassword } from "../helpers/authHelper";
 export const registerController = async (req,res) =>{
 try {
     const {name,email,password, phone,address}=req.body
@@ -21,14 +21,22 @@ try {
     }
     //checking for  user
     const existingUser = await userModel.findOne({email});
-    //checking existing user 
+    //checking for existing user 
     if(existingUser){
         return res.status(200).send({
             success:true,message:'user already exist please login'
         })
     }
     //register user
-    
+    const hashedPassword=  await hashedPassword(password)
+    //save it 
+    const user = new userModel({name,email,phone,address,password:hashedPassword}).save
+    res.status(201).send({
+        success : true,
+        message:'user successfully  register',
+        user
+    })
+
 } catch (error) {
     console.log(error);
     res.status(500).send({//status code 500 is for internal server error
--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";//bcrypt hides the password
+
 export const hashPassword = async (password)=>{
 try {
     const saltRounds = 10 ;
