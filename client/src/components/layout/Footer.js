11d7cbed100f0b1b28b7b4557f761ed0de2f875e
me-elsa <elsaa.butt@gmail.com>
2024-02-22 17:52:02 +0500

    pages and routing

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">
+        All Right Reserved <i class="fa-regular fa-copyright"></i>
+      </h4>
+    </div>
+  )
+}
+
+export default Footer
