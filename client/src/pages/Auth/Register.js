45371eadf0aa8a7d1276417626001c5ea4015d5a
me-elsa <elsaa.butt@gmail.com>
2024-03-30 16:29:58 +0500

    registration

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import Layout from '../../components/layout/Layout'
 import toast from "react-hot-toast"
 import {useNavigate} from "react-router-dom" //ek pg se dosaray pg per navigate kernay k liye
-
+import '../AuthStyle.css'
 import axios from "axios" //Axios is a JavaScript library used for making HTTP requests from web applications.
 const Register = () => {
     const[name,setName] = useState("")//first parameter getter 2nd is setter 
