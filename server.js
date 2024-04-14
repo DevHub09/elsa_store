b5c838def97634e13e0007bfffd4ea92d9f03f00
me-elsa <elsaa.butt@gmail.com>
2024-04-14 20:05:09 +0500

    protect dashboard page

--- server.js
+++ server.js
@@ -4,31 +4,28 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import cors from "cors"
+import cors from "cors";
 //config env (file for private keys of our project)
 dotenv.config();
-const port = process.env.port||8080;//default is 8080
+const port = process.env.port || 8080; //default is 8080
 //database config
 connectDB();
 const app = express();
+
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
-//routes 
-app.use('/api/v1/auth',authRoutes);
+app.use(morgan("dev"));
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest API
-app.get("/",(req,res)=>{
-    res.send({
-        message:"welcome to ecommer app",
-    });
+app.get("/", (req, res) => {
+  res.send({
+    message: "welcome to ecommer app",
+  });
 });
-//listen 
-app.listen(port,()=>{
-    console.log(`the server is running on port ${port} `.bgMagenta.white);
-
-})
-
-
-
+//listen
+app.listen(port, () => {
+  console.log(`the server is running on port ${port} `.bgMagenta.white);
+});
